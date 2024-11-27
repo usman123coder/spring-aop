b3002e9ef4905fdce622f5ff75ebedf9cb625a0a
Usman <usmankhan748596@gmail.com>
2024-11-27 12:57:00 +0530

    rechanges

--- throwsAdvice/src/main/java/com/ta/advice/ExceptionLoggingAdvice.java
+++ throwsAdvice/src/main/java/com/ta/advice/ExceptionLoggingAdvice.java
@@ -2,9 +2,18 @@ package com.ta.advice;
 
 import org.springframework.aop.ThrowsAdvice;
 
+import java.lang.reflect.Method;
+
 public class ExceptionLoggingAdvice implements ThrowsAdvice {
+   /*
     public void afterThrowing(IllegalArgumentException e){
         System.out.println("This is in throws advice class method");
         System.out.println("Exception has been throw with message : " + e.getMessage());
+    }
+   */
+    public void afterThrowing(Method method, Object[] args, Object target, IllegalArgumentException e){
+        System.out.println("from throws advice class method");
+        System.out.println("exception thrown by : " + method.getName() + "(" + args[0] + ") with message" +
+                e.getMessage());
     }
 }
